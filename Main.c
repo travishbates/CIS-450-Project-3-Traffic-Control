e1447cc75fbf4a2f98952c434d2e506540ba1e99
Travis Bates <travishbates@gmail.com>
2018-03-27 17:20:32 -0400

    global variables

--- Main.c
+++ Main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+const int DELTA_LEFT = 3, DELTA_STRAIGHT = 2, DELTA_RIGHT = 1;
+
 typedef struct _directions {
 	char dir_original;
 	char dir_target;
-} directions;void ArriveIntersection(directions dir) {}void CrossIntersection(directions dir) {}void ExitIntersection(directions dir) {}
+} directions;typedef struct _car {	int cid;	directions dir;	double arrival_time;} car;void ArriveIntersection(directions dir) {	}void CrossIntersection(directions dir) {}void ExitIntersection(directions dir) {}
 Car(directions dir) {
 	ArriveIntersection(dir);
 	CrossIntersection(dir);
