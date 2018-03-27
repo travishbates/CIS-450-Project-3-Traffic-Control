4f7b294c2affdbc2225395f2ea7128a641539f3d
Travis Bates <travishbates@gmail.com>
2018-03-27 16:49:51 -0400

    added required functions

--- Main.c
+++ Main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+typedef struct _directions {
+	char dir_original;
+	char dir_target;
+} directions;void ArriveIntersection(directions dir) {}void ExitIntersection(directions dir) {}
+Car(directions dir) {
+	ArriveIntersection(dir);
+	CrossIntersection(dir);
+	ExitIntersection(dir);
+}
 void main() {
-	//test
+	//test..
 	printf("Hello, world!\n");
 	system("pause");
 }
