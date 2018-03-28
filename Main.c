d6d0533133dcb9fea761d16a5feb1b8d7b184eb4
Travis Bates <travishbates@gmail.com>
2018-03-27 20:15:28 -0400

    more work, need to start on ptrheads

--- Main.c
+++ Main.c
@@ -7,27 +7,26 @@ const double DELTA_TICK = 0.1;
 
 double clock = 0.0;
 
-
 typedef struct _directions {
 	char dir_original;
 	char dir_target;
-} directions;typedef struct _car {	int cid;	double arrival_time;	directions dir;} car;void ArriveIntersection(directions dir) {	}void CrossIntersection(directions dir) {}void ExitIntersection(directions dir) {}
-Car(directions dir) {
-	ArriveIntersection(dir);
-	CrossIntersection(dir);
-	ExitIntersection(dir);
-}/*car GetCars() {	car carArray[NUM_CARS] = {		{0, 1.1, { 'N', 'N' }},		{1, 2.0, { 'N', 'N' }},		{2, 3.3, { 'N', 'N' }},		{3, 3.5, { 'N', 'N' }},		{4, 4.2, { 'N', 'N' }},		{5, 4.4, { 'N', 'N' }},		{6, 5.7, { 'N', 'N' }},		{7, 5.9, { 'N', 'N' }},	};	return carArray;}*/void tick() {	//check new cars entering	//follow instructions (semaphores) for	//passing thru intersection	//tick	clock += DELTA_TICK;}
+} directions;typedef struct _car {	int cid;	double arrival_time;	directions *dir;} car;void PrintTime() {	printf("Time %d: ", clock);}void PrintCar(directions *dir) {	printf("Car %d (->%c ->%c) ", dir->dir_original, dir->dir_target);}void PrintOutput(char *str, directions *dir) {	PrintTime();	PrintCar(dir);	printf(str);}void ArriveIntersection(directions *dir) {	PrintOutput("arriving", dir);	/*do logic here*/}void CrossIntersection(directions *dir) {	PrintOutput("crossing", dir);}void ExitIntersection(directions *dir) {	PrintOutput("exiting", dir);}
+Car(void* arg) {
+	car *threadcar = (car*)arg;
+
+	ArriveIntersection(threadcar->dir);
+	CrossIntersection(threadcar->dir);
+	ExitIntersection(threadcar->dir);
+}void tick() {	clock += DELTA_TICK;}car* GetCars() {	car *carArray = malloc(NUM_CARS * sizeof(car));	for (int i = 0; i < NUM_CARS; i++)		carArray[i].dir = malloc(sizeof(directions));	carArray[0].cid = 0;	carArray[1].cid = 1;	carArray[2].cid = 2;	carArray[3].cid = 3;	carArray[4].cid = 4;	carArray[5].cid = 5;	carArray[6].cid = 6;	carArray[7].cid = 7;	carArray[0].arrival_time = 1.1;	carArray[1].arrival_time = 2.0;	carArray[2].arrival_time = 3.3;	carArray[3].arrival_time = 3.5;	carArray[4].arrival_time = 4.2;	carArray[5].arrival_time = 4.4;	carArray[6].arrival_time = 5.7;	carArray[7].arrival_time = 5.9;	carArray[0].dir->dir_original = 'N';	carArray[1].dir->dir_original = 'N';	carArray[2].dir->dir_original = 'N';	carArray[3].dir->dir_original = 'S';	carArray[4].dir->dir_original = 'S';	carArray[5].dir->dir_original = 'N';	carArray[6].dir->dir_original = 'E';	carArray[7].dir->dir_original = 'W';	carArray[0].dir->dir_target = 'N';	carArray[1].dir->dir_target = 'N';	carArray[2].dir->dir_target = 'W';	carArray[3].dir->dir_target = 'S';	carArray[4].dir->dir_target = 'E';	carArray[5].dir->dir_target = 'N';	carArray[6].dir->dir_target = 'N';	carArray[7].dir->dir_target = 'N';		return carArray;}void freeCars(car* carArray) {	for (int i = 0; i < NUM_CARS; i++)		free(carArray[i].dir);	free(carArray);}
 void main() {
-	//car cars[NUM_CARS] = GetCars();
-
-	car cars[NUM_CARS] = {		{ 0, 1.1,{ 'N', 'N' } },		{ 1, 2.0,{ 'N', 'N' } },		{ 2, 3.3,{ 'N', 'N' } },		{ 3, 3.5,{ 'N', 'N' } },		{ 4, 4.2,{ 'N', 'N' } },		{ 5, 4.4,{ 'N', 'N' } },		{ 6, 5.7,{ 'N', 'N' } },		{ 7, 5.9,{ 'N', 'N' } },	};
-
-	while (1)
-		tick();
+	car* cars = GetCars();
 
 	for (int i = 0; i < NUM_CARS; i++)
-		printf("dir 1: %c dir 2: %c\n", cars[i].dir.dir_original, cars[i].dir.dir_target);
+		printf("dir 1: %c dir 2: %c\n", cars[i].dir->dir_original, cars[i].dir->dir_target);
 
-	printf("Hello, world!\n");
+	/*Thread stuff goes here*/
+
+	freeCars(cars);
+
 	system("pause");
 }
